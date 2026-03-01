5eb54357c4a1737634b36d176f2f8e7297ed20eb
shiroe <xxx@xxx>
2026-03-01 14:39:28 +0900

    feat: blank table

--- js/renderTable.2.js
+++ js/renderTable.2.js
@@ -4,16 +4,30 @@ export function renderTable(budget2,categories) {
     // @ts-ignore
     tbody.innerHTML = "";
     categories.forEach((cat, i) => {
-        const tr = document.createElement('tr');
-        const tdCat = document.createElement('td');
-        tdCat.textContent = cat;
+        {
+            const tr = document.createElement('tr');
+            const tdCat = document.createElement('td');
+            tdCat.textContent = cat;
+            tr.appendChild(tdCat);
+            tbody.appendChild(tr);
+        }
+
+        const tr2=document.createElement('tr');    
+        const tdBlank = document.createElement('td');
+        tr2.appendChild(tdBlank);
+
+        for(let j=0;j<5;j++){
+            createEditableCell(budget2, i, tr2);
+        }
+
+        tbody.appendChild(tr2);
+    });
+}
+
+function createEditableCell(budget2, i, tr2) {
         const tdVal = document.createElement('td');
         tdVal.textContent = budget2[i].toString();
         tdVal.contentEditable = 'true'; // 編集可能
         tdVal.addEventListener('input', () => budget2[i] = Number(tdVal.textContent));
-        tr.appendChild(tdCat);
-        tr.appendChild(tdVal);
-        // @ts-ignore
-        tbody.appendChild(tr);
-    });
+        tr2.appendChild(tdVal);
 }
